618c66df5d5d1be7ad300158ab2faeb24fead330
Mehmet <mehmetkuzulugil@gmail.com>
2022-10-10 16:00:35 +0300

    Tamamen selection üzerinden bilgi alınarak yapılıyor.

--- content.js
+++ content.js
@@ -25,51 +25,59 @@ function stripContent(el) {
 }
 
 
-function getTagValSol(tagName) {		
+function getTagValSol(selected, tagNames) {
+	var theArticle;
+	var range = selected.getRangeAt(0);
+	var articles = document.getElementsByTagName('article');
+	var retVals = new Array();
 
-	var all_divs = document.getElementsByTagName('div');
-	var divs = [];
-	var lastDiv;
+	for (art of articles) {
+		if (art.contains(range.startContainer)) {
+			theArticle = art;
+			break;
+		}
+	}
+	if (theArticle == null)
+		return null;
 
-	for (var i = 0; i < all_divs.length; i++) {
-		var div = all_divs[i];
-		var rg = new RegExp('single-\\w+-' +tagName,'g'); 
-		if (div.className.match(rg)) {
-			if (isInViewPort(div)){
-				return stripContent(div);
-			}
-			else {
-				lastDiv = div;
+	var all_divs = art.getElementsByTagName('div');
+	for (tagName of tagNames) {
+		for (var i = 0; i < all_divs.length; i++) {
+			var div = all_divs[i];
+			var rg = new RegExp('single-\\w+-' +tagName,'g'); 
+			if (div.className.match(rg)) {
+				retVals[tagName] = stripContent(div);
 			}
 		}
 	}
-	if (lastDiv != null) {
-		return stripContent(lastDiv);
-	}
-	else
-		return "...";
-
+	return retVals;
 }
 
 async function copyToTheClipboard(textToCopy){
-//	tmpText = getDate();
+	//	tmpText = getDate();
 	//	
+	var dateText = "";
+	var reporterText = "";
+	var titleText = "";
+
 	var sel = document.getSelection();
+	var tagVals = getTagValSol(sel, ['datetime', 'reporter', 'title']);
+	if (tagVals != null){
+		if ('datetime' in tagVals)
+			dateText = tagVals['datetime'];
+		if ('reporter' in tagVals)
+			reporterText = tagVals['reporter'];
+		if ('title' in tagVals)
+			titleText = tagVals['title'];
+	}
 
-	var dateText = getTagValSol("datetime");
-	var reporterText2 = getTagValSol("reporter");
-	//var urlText = document.URL;
 	var urlText = sel.baseNode.ownerDocument.URL;
-	var titleText = sel.baseNode.ownerDocument.title;
-	//var titleText = getTagValSol("title");
 
-	var tagText = "[" + dateText+ "] - " + reporterText2; // + " - " + titleText;
-
-	//var tagText = "[" + dateText+ "] - " + reporterText2 + " - " + titleText;
+	var tagText = "  [" + dateText+ "] - " + reporterText + "\n  " + titleText;
 
 	selText  = document.getSelection().toString();
 
-	sonText = selText + " \n" + tagText + "\n" + urlText + "\n";
+	sonText = selText + " \n" + tagText + "\n " + urlText + "\n";
 	//	sonText = "\"" + selText + "\"" + tagText + "\n";
 
 	const el = document.createElement('textarea');
